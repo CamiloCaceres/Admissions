cc4109384bb4240cf7035b16b7582b4e7b7ebb68
camilo caceres <camiloac97@gmail.com>
2024-03-10 21:42:49 -0500

    Feat: Add  default layout

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const links = [{
+  label: 'Notes',
+  icon: 'i-heroicons-pencil-square-solid',
+  to: '/notes'
+}, {
+  label: 'Playground',
+  icon: 'i-simple-icons-stackblitz',
+  to: '/playground'
+}, {
+  label: 'Roadmap',
+  icon: 'i-heroicons-map',
+  to: '/roadmap'
+}, {
+  label: 'Pro',
+  icon: 'i-heroicons-square-3-stack-3d',
+  to: '/pro'
+}, {
+  label: 'Releases',
+  icon: 'i-heroicons-rocket-launch',
+  to: 'https://github.com/nuxt/ui/releases',
+  target: '_blank'
+}]
+
+
+</script>
+
+<template>
+  <UHeader>
+    <template #logo>
+      <h1><span class="text-orange-500 ">T</span>Wiki</h1>
+    </template>
+    <template #right>
+      <UColorModeButton />
+    </template>
+  </UHeader>
+
+  <UContainer>
+    <UPage>
+      <template #left>
+        <UAside :links="links">
+
+        </UAside>
+      </template>
+
+      <slot />
+    </UPage>
+  </UContainer>
+</template>
---
+++ pages/notes.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const selectedTrimester = ref()
+const courseCode = ref('MITA21')
+
+</script>
+
+<template>
+ <UCard>
+      
+      <h2 class="text-xl font-semibold">Academic notes</h2>
+      <div class="flex space-x-5">
+        <div class="flex space-x-5"> <span>Trimester: </span>
+          <UInput v-model="selectedTrimester" />
+        </div>
+        <div class="flex space-x-5"><span>Course</span> <UInput v-model="courseCode" /> </div>
+      </div>
+
+
+      <span>Completed</span> <input type="text" name="YEARS" id=""> <span>Years of</span> <input type="text"
+        name="COURSE_COMPLETED" id="">
+      <span>at </span> <input type="text" name="COLLEGUE">
+  </UCard>
+
+</template>
